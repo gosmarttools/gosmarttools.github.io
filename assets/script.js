bfe03ed495e91f6d0b87c95ca3b226948b3ded88
GoSmart Tools <gosmarttools@gmail.com>
2026-02-22 14:31:32 +0700

    assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.getElementById("navLinks");
+  const navItems = document.querySelectorAll(".nav-links a");
 
+  // Toggle menu
   hamburger.addEventListener("click", function(){
     navLinks.classList.toggle("active");
+  });
+
+  // Auto close after click (mobile UX)
+  navItems.forEach(function(link){
+    link.addEventListener("click", function(){
+      navLinks.classList.remove("active");
+    });
   });
 
 });
